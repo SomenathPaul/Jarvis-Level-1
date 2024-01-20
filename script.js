31a064fa3043a4c30e1cf77c76ba2f0d7a297e38
Somenath Paul <141508667+SomenathPaul@users.noreply.github.com>
2024-01-20 21:13:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const content = document.querySelector(".content");
 function speak(text) {
   const text_speak = new SpeechSynthesisUtterance(text);
 
-  text_speak.rate = 1.7;
+  text_speak.rate = 1;
   text_speak.volume = 1;
   text_speak.pitch = 1;
 
